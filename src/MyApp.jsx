9683487e51d09185fb17f92f6a00caba029daac6
Stankevich Katsiaryna <stankevichk@p21706w.MINI>
2019-10-08 09:59:38 +0200

    Customize headline

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const App = () => (
-  <h1>Minimal React</h1>  
+  <h1>Minimal React stankevichk 77d7f835d61412e9dfa7047ef7b7dce05e1e2402</h1>  
 )
 
 export default App
