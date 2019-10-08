ea2d92f8be13551048cc6a028f878c3be84c82ca
Stankevich Katsiaryna <stankevichk@p21706w.MINI>
2019-10-08 10:09:01 +0200

    Customize headline

--- src/MyApp.jsx
+++ src/MyApp.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const App = () => (
-  <h1>Minimal React stankevichk 77d7f835d61412e9dfa7047ef7b7dce05e1e2402</h1>  
+  <h1>Minimal React stankevichk 
+    <p>Bundle size: 61 bytes, Load time of the bundle: 32 ms, Last commit SHA1: 77d7f835d61412e9dfa7047ef7b7dce05e1e2402</p>
+  </h1>  
+  
 )
 
 export default App
