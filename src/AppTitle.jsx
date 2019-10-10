4b7fe471886431d0c6f6bdc628692798fa186337
unknown <ostankevichea@gmail.com>
2019-10-10 20:06:47 +0200

    Lab1Ex3

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 const generateArray = (n) =>(
   Array.from({length: n}, (_, i) => i+1 )
   )
-
+const generateRandomArray = (n) =>(
+    Array.from({length: n}, () => Math.floor(Math.random() * 25 + 1))
+    )
 
 
 export default class AppTitle extends React.Component {
@@ -12,7 +14,7 @@ export default class AppTitle extends React.Component {
       <div>
          
         <p>Hello World!</p>
-            
+        <p>{generateRandomArray(5).toString()}</p>    
       </div>
       
     )
