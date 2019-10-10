605c698435e945bd235b2d0c3b65362777b5f9ff
unknown <ostankevichea@gmail.com>
2019-10-10 20:03:14 +0200

    Lab1Ex2

--- src/AppTitle.jsx
+++ src/AppTitle.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+const generateArray = (n) =>(
+  Array.from({length: n}, (_, i) => i+1 )
+  )
+
+
+
 export default class AppTitle extends React.Component {
  render() {
     return (
       <div>
+         
         <p>Hello World!</p>
+            
       </div>
+      
     )
   }
 }
--- src/main.js
+++ src/main.js
@@ -2,4 +2,11 @@ import React from 'react'
 import { render } from 'react-dom'
 import App from './MyApp'
 
-render(<App />, document.getElementById('reactinjectedhere'))
+
+
+render(<App />, 
+    
+    
+    
+    document.getElementById('reactinjectedhere')
+);
