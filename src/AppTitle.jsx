db33441769f3af5f3e503167f64b12fca3567c63
Stankevich Katsiaryna <stankevichk@p31120w.MINI>
2019-10-10 15:00:26 +0200

    App Title

---
+++ src/AppTitle.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class AppTitle extends React.Component {
+ render() {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    )
+  }
+}
